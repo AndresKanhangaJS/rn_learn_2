925f4a88db1929e90a933457d1135672ac90114c
AndresKanhangaJS <64920912+AndresKanhangaJS@users.noreply.github.com>
2024-05-23 07:31:06 +0100

    AVANÇANDO!!!

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { globalStyles, images } from '../styles/global';
+import Card from '../shared/card';
 
 export default function ReviewDetails({ route, navigation }) {
   const { title, body, rating } = route.params;
 
+  // const images = {
+  //   1: require('../assets/rating-1.png'),
+  //   2: require('../assets/rating-2.png'),
+  //   3: require('../assets/rating-3.png'),
+  //   4: require('../assets/rating-4.png'),
+  //   5: require('../assets/rating-5.png'),
+  // };
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>{title}</Text>
-      <Text>{body}</Text>
-      <Text>{rating}</Text>
+      <Card>
+        <Text style={globalStyles.titleText}>{title}</Text>
+        <Text>{body}</Text>
+        <View style={styles.rating}>
+          <Text>Game Rating:</Text>
+          <Image source={images.ratings[rating]} />
+        </View>
+      </Card>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee'
+  }
 });
--- shared/header.js
+++ shared/header.js
@@ -1,39 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
-export default function Header ({title}){
-    const navigation = useNavigation();
+export default function Header({ title }) {
+  const navigation = useNavigation();
 
-    const openMenu = () =>{
-        navigation.openDrawer()
-    }
-    return(
-        <View style={styles.header}>
-            <MaterialIcons name='menu' size={28} style={styles.icon} onPress={openMenu}/>
-            <View>
-                <Text style={styles.headerText} >{title}</Text>
-            </View>
-        </View>
-    );
+  const openMenu = () => {
+    navigation.openDrawer();
+  };
+
+  return (
+    <View style={styles.header}>
+      <MaterialIcons name="menu" size={28} style={styles.icon} onPress={openMenu} />
+      <View style={styles.headerTitle}>
+        <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
+        <Text style={styles.headerText}>{title}</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        height: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    headerText: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        color: '#333',
-        letterSpacing: 1
-    },
-    icon: {
-        position: 'absolute',
-        left: 16
-    }
-  });
+  header: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#333',
+    letterSpacing: 1,
+  },
+  icon: {
+    position: 'absolute',
+    left: 16,
+  },
+  headerTitle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginRight: 10,
+  },
+});
